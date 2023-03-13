5cf84100e89e9b2ce373275d59dbf0ccc74447d9
victor-alvescv <victoralvescabral7@gmail.com>
2023-03-13 18:45:20 -0300

    Init commit

---
+++ src/data.js
@@ -0,0 +1,111 @@
+// FAKE DATA
+export const books = [
+  {
+    id: 1,
+    title: "Crack the Coding Interview",
+    url: "https://covers.openlibrary.org/b/id/8091016-L.jpg",
+    originalPrice: 49.95,
+    salePrice: 14.95,
+    rating: 4.5,
+    para: "If you're looking for random paragraphs, you've come to the right place. When a random word or a random sentence isn't quite enough, the next logical step is to find a random paragraph. We created the Random Paragraph Generator with you in mind. The process is quite simple. Choose the number of random paragraphs you'd like to see and click the button. Your chosen number of paragraphs will instantly appear."
+  },
+  {
+    id: 2,
+    title: "Atomic Habits",
+    url: "https://covers.openlibrary.org/b/id/10958382-L.jpg",
+    originalPrice: 39,
+    salePrice: null,
+    rating: 5,
+    para: "I recollect that my first exploit in squirrel-shooting was in a grove of tall walnut-trees that shades one side of the valley. I had wandered into it at noontime, when all nature is peculiarly quiet, and was startled by the roar of my own gun, as it broke the Sabbath stillness around and was prolonged and reverberated by the angry echoes."
+  },
+  {
+    id: 3,
+    title: "Can't Hurt Me",
+    url: "https://covers.openlibrary.org/b/id/10425061-L.jpg",
+    originalPrice: 29,
+    salePrice: null,
+    rating: 5,
+    para: "A random paragraph can also be an excellent way for a writer to tackle writers' block. Writing block can often happen due to being stuck with a current project that the writer is trying to complete. By inserting a completely random paragraph from which to begin, it can take down some of the issues that may have been causing the writers' block in the first place."
+  },
+  {
+    id: 4,
+    title: "Deep Work",
+    url: "https://covers.openlibrary.org/b/id/10088428-L.jpg",
+    originalPrice: 44,
+    salePrice: 19,
+    rating: 4.5,
+    para: "Another productive way to use this tool to begin a daily writing routine. One way is to generate a random paragraph with the intention to try to rewrite it while still keeping the original meaning. The purpose here is to just get the writing started so that when the writer goes onto their day's writing projects, words are already flowing from their fingers."
+  },
+  {
+    id: 5,
+    title: "The 10X Rule",
+    url: "https://covers.openlibrary.org/b/id/9978588-L.jpg",
+    originalPrice: 32,
+    salePrice: null,
+    rating: 5,
+    para: "Another writing challenge can be to take the individual sentences in the random paragraph and incorporate a single sentence from that into a new paragraph to create a short story. Unlike the random sentence generator, the sentences from the random paragraph will have some connection to one another so it will be a bit different. You also won't know exactly how many sentences will appear in the random paragraph."
+  },
+  {
+    id: 6,
+    title: "Sell Or Be Sold",
+    url: "https://covers.openlibrary.org/b/id/7737110-L.jpg",
+    originalPrice: 70,
+    salePrice: 12.5,
+    rating: 5,
+    para: "No! All of the paragraphs in the generator are written by humans, not computers. When first building this generator we thought about using computers to generate the paragraphs, but they weren't very good and many times didn't make any sense at all. We therefore took the time to create paragraphs specifically for this generator to make it the best that we could"
+  },
+  {
+    id: 7,
+    title: "Rich Dad Poor Dad",
+    url: "https://covers.openlibrary.org/b/id/2380224-L.jpg",
+    originalPrice: 11,
+    salePrice: 10,
+    rating: 4,
+    para: "There are usually about 200 words in a paragraph, but this can vary widely. Most paragraphs focus on a single idea that's expressed with an introductory sentence, then followed by two or more supporting sentences about the idea. A short paragraph may not reach even 50 words while long paragraphs can be over 400 words long, but generally speaking they tend to be approximately 200 words in length."
+  },
+  {
+    id: 8,
+    title: "Cashflow Quadrant",
+    url: "https://covers.openlibrary.org/b/id/1954899-L.jpg",
+    originalPrice: 38,
+    salePrice: 17.95,
+    rating: 4.5,
+    para: "The choice was red, green, or blue. It didn't seem like an important choice when he was making it, but it was a choice nonetheless. Had he known the consequences at that time, he would likely have considered the choice a bit longer. In the end, he didn't and ended up choosing blue."
+  },
+  {
+    id: 9,
+    title: "48 Laws of Power Summary",
+    url: "https://covers.openlibrary.org/b/id/8906626-L.jpg",
+    originalPrice: 35,
+    salePrice: 19.95, 
+    rating: 4.5,
+    para: "He wondered if he should disclose the truth to his friends. It would be a risky move. Yes, the truth would make things a lot easier if they all stayed on the same page, but the truth might fracture the group leaving everything in even more of a mess than it was not telling the truth. It was time to decide which way to go."
+  },
+  {
+    id: 10,
+    title: "The 5 Second Rule",
+    url: "https://covers.openlibrary.org/b/id/8114155-L.jpg",
+    originalPrice: 40,
+    salePrice: null,
+    rating: 5,
+    para: "Cake or pie? I can tell a lot about you by which one you pick. It may seem silly, but cake people and pie people are really different. I know which one I hope you are, but that's not for me to decide. So, what is it? Cake or pie?"
+  },
+  {
+    id: 11,
+    title: "How to Win Friends & Influence People",
+    url: "https://covers.openlibrary.org/b/id/7895280-L.jpg",
+    originalPrice: 30,
+    salePrice: 20,
+    rating: 5,
+    para: "Can I get you anything else? David asked. It was a question he asked a hundred times a day and he always received the same answer. It had become such an ingrained part of his daily routine that he had to step back and actively think when he heard the little girl's reply. Nobody had before answered the question the way that she did, and David didn't know how he should respond."
+  },
+  {
+    id: 12,
+    title: "Mastery",
+    url: "https://covers.openlibrary.org/b/id/8479576-L.jpg",
+    originalPrice: 30,
+    salePrice: 12.95,
+    rating: 4.5,
+    para: "You're going to make a choice today that will have a direct impact on where you are five years from now. The truth is, you'll make choice like that every day of your life. The problem is that on most days, you won't know the choice you make will have such a huge impact on your life in the future. So if you want to end up in a certain place in the future, you need to be careful of the choices you make today."
+  },
+];
